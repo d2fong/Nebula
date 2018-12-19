5b6f6fa15e67fcc4230fbaf4d3b19f35f4a1948a
Dylan Fong <dylanfong.ut@gmail.com>
2018-12-19 15:12:06 -0500

    include stop words, filtering by occurences

--- src/index.js
+++ src/index.js
@@ -2,28 +2,56 @@ const assign = require('lodash.assign');
 const uniq = require('lodash.uniq');
 
 const { stemmer } = require('porter-stemmer');
+const stopWords = require('./stopwords.json');
+const flaggedWords = require('./flaggedwords.json');
 
 let textCharacterization = (text, opts) => {
-  let { stopWords, flaggedWords, useStemming } = opts;
   let delimiterRegex = /[\t \n\r\f!\\#$%&()*+,.<=>?@[^\\\]`_{|}~\\'"]/;
   let filterWords = new Set([...stopWords, flaggedWords]);
   let wordPairMap = new Map();
+  let wordOccurenceMap = new Map();
+  let stemToWordsMap = new Map();
 
-  let words = uniq(text.split(delimiterRegex).filter( word => !filterWords.has(word) ).map( word => stemmer(word) ) );
+  let incrKey = ( m, k ) => {
+    if( m.has( k ) ){
+      m.set(k, m.get(k) + 1);
+    } else {
+      m.set( k, 1 );
+    }
+  };
+  let appendKey = ( m, k, v ) => {
+    console.log( k, v);
+    if( m.has( k ) ){
+      m.set(k, m.get(k).add(v) );
+    } else {
+      m.set( k, new Set([v]) );
+    }
+  };
+
+  let wordStems = uniq(text.split(delimiterRegex).filter( word => !filterWords.has(word) ).map( word => {
+    incrKey( wordOccurenceMap, word );
+    let wordStem = stemmer(word);
+    appendKey( stemToWordsMap, wordStem, word );
+    return wordStem;
+   } ) );
 
 
-  for( let i = 0; i < words.length - 1; i++ ){
-    for( let j = 1; j < words.length -1; j++ ){
-      let wordPair = new Set(words[i], words[j]);
-      if( wordPairMap.has(wordPair) ){
-        wordPairMap.set(wordPair, wordPairMap.get(wordPair) + 1);
-      } else {
-        wordPairMap.set(wordPair, 1);
-      }
+  for( let i = 0; i < wordStems.length - 1; i++ ){
+    for( let j = 1; j < wordStems.length -1; j++ ){
+      let wordPair = new Set(wordStems[i], wordStems[j]);
+      incrKey( wordPairMap, wordPair );
     }
   }
 
-  return words;
+  let words = wordStems.map( ws => {
+    let associatedWords = [...stemToWordsMap.get( ws )].sort( (w0, w1 ) => {
+      return wordOccurenceMap.get(w1) > wordOccurenceMap.get(w0);
+    } );
+
+    return associatedWords[0];
+  });
+
+  return words.filter( word => wordOccurenceMap.get(word) > 1 );
 };
 
 
@@ -41,4 +69,4 @@ let entry = (text, opts) => {
 
 entry(`
 signal transduction involved in cell cycle checkpoint. signal transduction involved in DNA integrity checkpoint. signal transduction involved in mitotic cell cycle checkpoint. signal transduction involved in mitotic DNA integrity checkpoint. positive regulation of cell cycle arrest. G1 DNA damage checkpoint. mitotic DNA damage checkpoint. signal transduction involved in DNA damage checkpoint. signal transduction involved in mitotic DNA damage checkpoint. intracellular signal transduction involved in G1 DNA damage checkpoint. mitotic G1/S transition checkpoint. mitotic G1 DNA damage checkpoint. signal transduction involved in mitotic G1 DNA damage checkpoint. DNA damage response, signal transduction by p53 class mediator resulting in cell cycle arrest
-`, {});
+`, {})
